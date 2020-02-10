b13cdc4910e0788e7d8c0ef0f2300978391e26b9
tiasma92 <mattias.azoulay@gmail.com>
2020-02-10 14:23:44 +0100

    test

--- Les Calanques/Controller/FirstViewController.swift
+++ Les Calanques/Controller/FirstViewController.swift
@@ -12,6 +12,7 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("salut")
         // Do any additional setup after loading the view.
     }
 
